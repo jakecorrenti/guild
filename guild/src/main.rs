f9ce2c6f5b8641376862f816f4c43b4b947f0830
jakecorrenti <jcorrenti13@outlook.com>
2020-12-28 17:58:19 -0500

    Have basic CLI usage configuration done.

--- guild/src/main.rs
+++ guild/src/main.rs
@@ -1,3 +1,9 @@
+#[macro_use]
+extern crate clap;
+use clap::App;
+
 fn main() {
+    let yaml = load_yaml!("cli.yml");
+    let matches = App::from_yaml(yaml).get_matches();
     println!("Hello, world!");
 }
--- guild/target/debug/deps/guild.d
+++ guild/target/debug/deps/guild.d
@@ -1,5 +1,6 @@
-/Users/jakecorrenti/guild/guild/target/debug/deps/guild: src/main.rs
+/Users/jakecorrenti/guild/guild/target/debug/deps/guild: src/main.rs src/cli.yml
 
-/Users/jakecorrenti/guild/guild/target/debug/deps/guild.d: src/main.rs
+/Users/jakecorrenti/guild/guild/target/debug/deps/guild.d: src/main.rs src/cli.yml
 
 src/main.rs:
+src/cli.yml:
