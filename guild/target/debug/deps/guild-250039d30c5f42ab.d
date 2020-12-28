46e9234f087786d97552115066fcab87fc673424
jakecorrenti <jcorrenti13@outlook.com>
2020-12-28 18:26:57 -0500

    Fixed issues where adding highlight flag was not working

---
+++ guild/target/debug/deps/guild-250039d30c5f42ab.d
@@ -0,0 +1,6 @@
+/Users/jakecorrenti/guild/guild/target/debug/deps/guild-250039d30c5f42ab.rmeta: src/main.rs src/cli.yml
+
+/Users/jakecorrenti/guild/guild/target/debug/deps/guild-250039d30c5f42ab.d: src/main.rs src/cli.yml
+
+src/main.rs:
+src/cli.yml:
