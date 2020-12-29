e7fea4fd872499d70e38b6a6c198309f5c8ea5d0
jakecorrenti <jcorrenti13@outlook.com>
2020-12-29 00:12:15 -0500

    File parsing to get the code snippet from the specified file

--- guild/src/lib.rs
+++ guild/src/lib.rs
@@ -1,5 +1,40 @@
 use clap::ArgMatches;
+use std::{fs, process};
 
-pub fn run(args: &ArgMatches) {
+pub fn run(args: &ArgMatches) -> Result<(), Box<dyn std::error::Error>> {
 
+    /*
+     * can unwrap these four because they are all required in order for parsing to be valid. If
+     * they are not provided, the parser will panick and provide a default error message
+     */
+    let post_matches = args.subcommand_matches("post").unwrap();
+    let filename = post_matches.value_of("filename").unwrap();
+    let start = post_matches.value_of("start").unwrap();
+    let end = post_matches.value_of("end").unwrap();
+
+    // TODO 
+    if let Some(should_highlight) = post_matches.value_of("hl") {}
+
+    let file = fs::read_to_string(&filename)?;
+    let file_contents_per_line: Vec<&str> = file.lines().collect();
+    let start_as_int = start.parse::<i32>().unwrap();
+    let end_as_usize = end.parse::<usize>().unwrap();
+
+    if start_as_int <= 0 || end_as_usize > file_contents_per_line.len() {
+        eprintln!("The line range provided is invalid.");
+        process::exit(1);
+    }
+
+    let mut snippet_lines = Vec::new();
+    for (idx, line) in file_contents_per_line.iter().enumerate() {
+        if idx < end_as_usize {
+            snippet_lines.push(line);
+        }
+    }
+
+    for line in snippet_lines {
+        println!("{}", line);
+    }
+
+    Ok(())
 }
--- guild/src/main.rs
+++ guild/src/main.rs
@@ -2,9 +2,9 @@
 extern crate clap;
 use clap::App;
 
-fn main() {
+fn main() -> Result<(), Box<dyn std::error::Error>> {
     let yaml = load_yaml!("cli.yml");
     let matches = App::from_yaml(yaml).get_matches();
 
-    guild::run(&matches);
+    guild::run(&matches)
 }
