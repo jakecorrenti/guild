6a6f6eb8ed1fa223bab29a6a5499196c4a5f1e04
= <=>
2021-01-02 00:01:41 -0500

    cargo publish config

---
+++ guild/target/debug/deps/guild-30959ea0bd0ddd07.d
@@ -0,0 +1,6 @@
+/Users/jakecorrenti/guild/guild/target/debug/deps/guild-30959ea0bd0ddd07: src/main.rs src/cli.yml
+
+/Users/jakecorrenti/guild/guild/target/debug/deps/guild-30959ea0bd0ddd07.d: src/main.rs src/cli.yml
+
+src/main.rs:
+src/cli.yml:
